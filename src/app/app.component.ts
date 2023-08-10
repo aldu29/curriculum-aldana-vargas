b91303f7dde7d1e84021346215ed6a13df14d9a8
Aldana Vargas <alddu.av@gmail.com>
2023-08-10 14:51:35 -0300

    se separo en componentes

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'holamundo';
+  foto ='../assets/recursos/gato1.jpg';
+  foto2 ='../assets/recursos/gato2.jpg';
+  foto3 ='../assets/recursos/gato3.jpg';
+  foto4 ='../assets/recursos/gato4.jpg';
+   
+
+  
+}
