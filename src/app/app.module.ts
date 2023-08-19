9897170ea92195ca25b63374e6adb0a2d58d90a4
Aldana Vargas <alddu.av@gmail.com>
2023-08-19 19:11:31 -0300

    bootstrap, css y angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { SectionComponent } from './cv/section/section.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { DatosPersonalesComponent } from './datos-personales/datos-personales.component';
+import { ExperienciaLaboralComponent } from './experiencia-laboral/experiencia-laboral.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { DatosAcademicosComponent } from './datos/datos-academicos/datos-academicos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SectionComponent,
-    FooterComponent
+    FooterComponent,
+    DatosPersonalesComponent,
+    PresentacionComponent,
+    DatosAcademicosComponent,
+    ExperienciaLaboralComponent,
   ],
   imports: [
     BrowserModule
